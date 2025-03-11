235b7297f4e473ae0cc406daf1e8722c917238e6
Matthieu Grauleau <m.grauleau@gmail.com>
2025-03-11 16:34:10 +0100

    ajout de contact et icones

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import { useIntersectionObserver } from "../../utils/animationUtils";
 import "./contact.scss";
 
+// Import des icônes Font Awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faMapMarkerAlt, 
+  faPhone, 
+  faClock, 
+  faLocationDot, 
+  faPhoneVolume, 
+  faCalendarAlt, 
+  faHashtag
+} from '@fortawesome/free-solid-svg-icons';
+// Pour Instagram et Facebook qui sont des marques
+import { faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+
 function Contact() {
   const [titleRef, isTitleVisible] = useIntersectionObserver();
   const [mapRef, isMapVisible] = useIntersectionObserver();
@@ -38,31 +52,37 @@ function Contact() {
         >
           <div className="info-item">
             <div className="info-icon">
-              <i className="fas fa-map-marker-alt"></i>
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
             </div>
             <div className="info-content">
-              <h3>Adresse</h3>
+              <h3>
+                <FontAwesomeIcon icon={faLocationDot} /> Adresse
+              </h3>
               <p>7 rue Bon Secours<br/>44000 Nantes<br/>Arrêt de tram: Bouffay</p>
             </div>
           </div>
           
           <div className="info-item">
             <div className="info-icon">
-              <i className="fas fa-phone"></i>
+              <FontAwesomeIcon icon={faPhone} />
             </div>
             <div className="info-content">
-              <h3>Téléphone</h3>
+              <h3>
+                <FontAwesomeIcon icon={faPhoneVolume} /> Téléphone
+              </h3>
               <p><a href="tel:0622323872">06 22 32 38 72</a></p>
             </div>
           </div>
           
           <div className="info-item">
             <div className="info-icon">
-              <i className="fas fa-clock"></i>
+              <FontAwesomeIcon icon={faClock} />
             </div>
             <div className="info-content">
-              <h3>Horaires</h3>
-              <p>Du Ludni au Dimanche<br/>De 11h30 à 15h</p>
+              <h3>
+                <FontAwesomeIcon icon={faCalendarAlt} /> Horaires
+              </h3>
+              <p>Du Lundi au Dimanche<br/>De 11h30 à 15h</p>
             </div>
           </div>
           
@@ -70,13 +90,15 @@ function Contact() {
             ref={socialRef}
             className={`social-links ${isSocialVisible ? 'animate-fadeInUp' : 'invisible'}`}
           >
-            <h3>Suivez-nous</h3>
+            <h3>
+              <FontAwesomeIcon icon={faHashtag} /> Suivez-nous
+            </h3>
             <div className="social-icons">
               <a href="https://www.instagram.com/mimosanantes" target="_blank" rel="noopener noreferrer" className="social-icon">
-                <i className="fab fa-instagram"></i>
+                <FontAwesomeIcon icon={faInstagram} />
               </a>
               <a href="https://www.facebook.com/mimosanantes" target="_blank" rel="noopener noreferrer" className="social-icon">
-                <i className="fab fa-facebook-f"></i>
+                <FontAwesomeIcon icon={faFacebookF} />
               </a>
             </div>
           </div>
