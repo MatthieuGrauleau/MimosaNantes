794c7c92589a39d92fd1ee2a3f39c763bc4de239
Matthieu Grauleau <m.grauleau@gmail.com>
2025-03-12 12:47:58 +0100

    about responsive

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import audrey from "../../assets/img/audrey.jpg";
 import creation from "../../assets/img/creation.JPG";
 import { useIntersectionObserver } from "../../utils/animationUtils";
@@ -7,6 +7,25 @@ function About() {
     const [leftContainerRef, isLeftVisible] = useIntersectionObserver();
     const [centerContainerRef, isCenterVisible] = useIntersectionObserver();
     const [rightContainerRef, isRightVisible] = useIntersectionObserver();
+    const [isMobile, setIsMobile] = useState(false);
+
+    // Détecte si l'écran est en mode mobile
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+        
+        // Vérification initiale
+        checkScreenSize();
+        
+        // Ajouter un écouteur d'événement pour le redimensionnement
+        window.addEventListener('resize', checkScreenSize);
+        
+        // Nettoyage de l'écouteur d'événement
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    }, []);
 
     return (
         <section className="about" id="about">
@@ -14,11 +33,15 @@ function About() {
                 ref={leftContainerRef}
                 className={`left-container ${isLeftVisible ? 'animate-fadeInLeft' : 'invisible'}`}
             >
-                <img src={audrey} alt="Audrey la patronne de l'établissement" />
+                <img 
+                    src={audrey} 
+                    alt="Audrey la patronne de l'établissement" 
+                    loading="lazy"
+                />
             </div>
             <div 
                 ref={centerContainerRef}
-                className={`center-container ${isCenterVisible ? 'animate-fadeInUp' : 'invisible'}`}
+                className={`center-container ${isCenterVisible ? (isMobile ? 'animate-fadeInUp' : 'animate-fadeInUp') : 'invisible'}`}
             >
                 <div>
                     <h1>Notre <span className="highlight_title">Histoire</span></h1>
@@ -34,7 +57,11 @@ function About() {
                 ref={rightContainerRef}
                 className={`right-container ${isRightVisible ? 'animate-fadeInRight' : 'invisible'}`}
             >
-                <img src={creation} alt="Audrey la patronne de l'établissement" />
+                <img 
+                    src={creation} 
+                    alt="Une création culinaire d'Audrey" 
+                    loading="lazy"
+                />
             </div>
         </section>
     );
