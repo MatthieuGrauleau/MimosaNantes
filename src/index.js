106df001867ee73d94fc83c2a9cc2b8476ffa118
Matthieu Grauleau <m.grauleau@gmail.com>
2025-03-13 11:02:54 +0100

    changer titre et logo

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import { router } from "./router/router.jsx";
 import { RouterProvider } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import logo from "./assets/img/logo.png";
 import "./sass/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <Helmet>
+      <title>Mimosa • Brunch</title>
+      <link rel="icon" href={logo} />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
